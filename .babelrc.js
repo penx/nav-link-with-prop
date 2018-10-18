bc0caa7b70c2a1f4140e4e850589ec61f8f3d691
Alasdair McLeay <alasdair.mcleay2@ibm.com>
2018-10-18 20:28:44 +0100

    babel 7

--- .babelrc.js
+++ .babelrc.js
@@ -6,11 +6,25 @@ const presets = [
 ];
 
 const plugins =  [
-  "transform-decorators-legacy",
-  ["transform-react-remove-prop-types"],
+  "@babel/plugin-proposal-object-rest-spread",
+  "@babel/plugin-proposal-class-properties",
+  // "@babel/plugin-proposal-decorators",
+  "@babel/plugin-proposal-do-expressions",
+  "@babel/plugin-proposal-export-default-from",
+  "@babel/plugin-proposal-export-namespace-from",
+  "@babel/plugin-proposal-function-sent",
+  "@babel/plugin-proposal-json-strings",
+  "@babel/plugin-proposal-logical-assignment-operators",
+  "@babel/plugin-proposal-nullish-coalescing-operator",
+  "@babel/plugin-proposal-numeric-separator",
+  "@babel/plugin-proposal-optional-chaining",
+  // "@babel/plugin-proposal-pipeline-operator",
+  "@babel/plugin-proposal-throw-expressions",
+  "@babel/plugin-syntax-dynamic-import",
+  "@babel/plugin-syntax-import-meta",
   [
     "@babel/plugin-transform-runtime",
-    { "helpers": false, "polyfill": false, "regenerator": true }
+    { "helpers": false, /*"polyfill": false,*/ "regenerator": true }
   ]
 ];
 
