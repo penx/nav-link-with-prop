456b212be83e22f6c570eb04757f6e744505fbe4
Alasdair McLeay <alasdair.mcleay2@ibm.com>
2018-10-18 21:07:09 +0100

    trim down NavLink and replace with render props

--- src/index.js
+++ src/index.js
@@ -2,24 +2,14 @@ import React from "react";
 import { Link, Route } from "react-router-dom";
 import PropTypes from "prop-types";
 
-
-function joinClassnames(...classnames) {
-  return classnames.filter(i => i).join(" ");
-}
-
 /**
- * A <Link> wrapper that knows if it's "active" or not.
+ * A Render Props wrapper that knows if it's "active" or not.
  */
 function NavLink({
-  "aria-current": ariaCurrent,
-  activeClassName,
-  activeStyle,
-  className: classNameProp,
   exact,
   isActive: isActiveProp,
   location,
   strict,
-  style: styleProp,
   to,
   ...rest
 }) {
@@ -39,52 +29,16 @@ function NavLink({
           ? isActiveProp(match, location)
           : match);
 
-        const className = isActive
-          ? joinClassnames(classNameProp, activeClassName)
-          : classNameProp;
-        const style = isActive ? { ...styleProp, ...activeStyle } : styleProp;
-
         return (
-          <Link
-            aria-current={(isActive && ariaCurrent) || null}
-            className={className}
-            style={style}
-            to={to}
-            {...rest}
-          />
+          this.children({
+            isActive,
+            to,
+            ...rest
+          })
         );
       }}
     />
   );
-}
-
-NavLink.defaultProps = {
-  "aria-current": "page",
-  activeClassName: "active"
-};
-
-if (__DEV__) {
-  const ariaCurrentType = PropTypes.oneOf([
-    "page",
-    "step",
-    "location",
-    "date",
-    "time",
-    "true"
-  ]);
-
-  NavLink.propTypes = {
-    "aria-current": ariaCurrentType,
-    activeClassName: PropTypes.string,
-    activeStyle: PropTypes.object,
-    className: PropTypes.string,
-    exact: Route.propTypes.exact,
-    isActive: PropTypes.func,
-    location: PropTypes.object,
-    strict: Route.propTypes.strict,
-    style: PropTypes.object,
-    to: Link.propTypes.to
-  };
 }
 
 export default NavLink;
