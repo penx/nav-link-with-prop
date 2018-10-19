652d32f1295f01fbb9eb9cbcea1028e4051c08fb
Alasdair McLeay <alasdair.mcleay2@ibm.com>
2018-10-19 14:23:24 +0100

    tests and readme update

--- README.md
+++ README.md
@@ -5,3 +5,49 @@ React Router NavLink that uses an active prop rather than css class.
 see https://github.com/UKHomeOffice/govuk-react/issues/423
 
 e.g. when you want to use a CSSinJS library that expects an active prop but you're using React Router 😢
+
+## Usage
+
+`npm install nav-link-with-prop --save`
+
+### Your Dumb Component
+
+Say you have a dumb/UI/styled component, such as:
+
+```jsx
+const MyNavAnchor = styled(({
+  as: T = 'a',
+  ...props
+}) => <T {...props} />)({
+  textDecoration: 'blink',
+  color: 'blue',
+}, ({ active }) => (active && {
+  color: 'red',
+}));
+```
+
+Note that you need to provide the 'as' prop. This is provided by styled-components by default but needs to be done manually (as per code sample above) for emotion.
+
+You can then use one of the following approaches:
+
+### Higher order component
+```jsx
+import { asNavLink } from 'nav-link-with-prop';
+
+const MyNavLink = asNavLink(config)(MyNavAnchor);
+```
+
+`config` is optional and can include an `isActive` function (as per ReactRouter's NavLink) and an `activeProp` string (the prop name that is passed to your dumb component).
+
+### Render Props
+A bit uglier:
+
+```jsx
+import NavLinkWithProp from 'nav-link-with-prop';
+
+const MyNavLink = ({ children, ...props }) => (
+  <NavLinkWithProp {...props}>
+    {innerProps => <MyNavAnchor as={Link} {...innerProps}>{children}</MyNavAnchor>}
+  </NavLinkWithProp>
+);
+```
--- src/as-nav-link.test.js
+++ src/as-nav-link.test.js
@@ -5,20 +5,21 @@ import renderer from 'react-test-renderer';
 import asNavLink from './as-nav-link';
 import 'jest-styled-components';
 
+const MyNavAnchor = styled(({
+  as: T = 'a',
+  ...props
+}) => <T {...props} />)({
+  textDecoration: 'blink',
+  color: 'blue',
+}, ({ active }) => (active && {
+  color: 'red',
+}));
+
 describe('asNavLink', () => {
   it('renders a styled-component without an active prop', () => {
-    const MyAnchor = styled(({
-      as: T = 'a',
-      ...props
-    }) => <T {...props} />)({
-      textDecoration: 'blink',
-      color: 'blue',
-    }, ({ active }) => (active && {
-      color: 'red',
-    }));
-    const MyLink = asNavLink()(MyAnchor);
+    const MyNavLink = asNavLink()(MyNavAnchor);
 
-    const MyTest = () => <Router><MyLink to="/somewhere">My Link</MyLink></Router>;
+    const MyTest = () => <Router><MyNavLink to="/somewhere">My Link</MyNavLink></Router>;
     const tree = renderer.create(<MyTest />).toJSON();
     expect(tree).toHaveStyleRule('color', 'blue');
     expect(tree.props.href === '/somewhere');
@@ -27,18 +28,9 @@ describe('asNavLink', () => {
   });
 
   it('renders a styled-component with an active prop', () => {
-    const MyAnchor = styled(({
-      as: T = 'a', // TODO: refactor, styled-components supports as prop already
-      ...props
-    }) => <T {...props} />)({
-      textDecoration: 'blink',
-      color: 'blue',
-    }, ({ active }) => (active && {
-      color: 'red',
-    }));
-    const MyLink = asNavLink()(MyAnchor);
+    const MyNavLink = asNavLink()(MyNavAnchor);
 
-    const MyTest = () => <Router><MyLink to="/">My Link</MyLink></Router>;
+    const MyTest = () => <Router><MyNavLink to="/">My Link</MyNavLink></Router>;
     const tree = renderer.create(<MyTest />).toJSON();
     expect(tree).toHaveStyleRule('color', 'red');
     expect(tree.props.href === '/');
@@ -47,8 +39,28 @@ describe('asNavLink', () => {
   });
 });
 
+it('renders a styled-component with a custom active prop', () => {
+  const MyCustomNavAnchor = styled(({
+    as: T = 'a',
+    ...props
+  }) => <T {...props} />)({
+    textDecoration: 'blink',
+    color: 'blue',
+  }, ({ custom }) => (custom && {
+    color: 'red',
+  }));
+
+  const MyNavLink = asNavLink({ activeProp: 'custom' })(MyCustomNavAnchor);
+
+  const MyTest = () => <Router><MyNavLink to="/">My Link</MyNavLink></Router>;
+  const tree = renderer.create(<MyTest />).toJSON();
+  expect(tree).toHaveStyleRule('color', 'red');
+  expect(tree.props.href === '/');
+  expect(tree.props.custom === true);
+  expect(tree.props.children === 'My Link');
+});
+
 // TODO:
 // - test for exact prop
 // - test for both emotion and styled-components
 // - asNavLink hof hoc, here to begin with then separate repo
-// - test for custom activeProp
